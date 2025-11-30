a2127d81a3ce29111fd17483700f61a47680bb40
rohit <rohitbhatt@q2pay.in>
2025-11-30 22:58:43 +0530

    wip: 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='bg-green-700 w-[200px]'><h1 style={{border:"2px solid red"}}>cfdgdg</h1></div>
   );
 }
 
